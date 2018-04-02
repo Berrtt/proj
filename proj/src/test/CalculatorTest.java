6564fc1735d60158a348b4a91456edad8552e537
agaliullin <agaliullin@agaliullin-pc.datateh.ru>
2018-04-02 18:35:19 +0500

    Небольшие правки

--- proj/src/test/CalculatorTest.java
+++ proj/src/test/CalculatorTest.java
@@ -12,22 +12,19 @@ class CalculatorTest {
 	@Test
 	void testCalcR() {
 		Calculator calculator = new Calculator();
-		calculator.setX(8);
-		calculator.setY(100);
+		calculator.setX(8);//расход на 100км
+		calculator.setY(100);//расстоояние
 		float r = calculator.calcR();
-		assertEquals(r, 8);
+		assertEquals(r, 8);//результат, который должен быть
 	}
 
 	@Test
 	void testCalcS() {
 		Calculator calculator = new Calculator();
-		calculator.setZ(40);
-		float r = 8;
-		//float z = 40;
-		//float calcR = 8;
+		calculator.setZ(40);//стоимость за 1 литр
+		float r = 8;//расход на указанное расстояние
 		float s = calculator.calcS(r);
-		//float s = r*z;
-		assertEquals(s, 320);
+		assertEquals(s, 320);//результат, который должен быть
 	}
 
 }
